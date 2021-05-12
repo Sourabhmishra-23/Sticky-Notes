22b8d1a468023c563eb62625cc73ce0fb66a37bf
Sourabh Mishra <59117474+Sourabhmishra-23@users.noreply.github.com>
2021-05-13 00:29:08 +0530

    update stickyNotes.js

--- js/stickyNotes.js
+++ js/stickyNotes.js
@@ -45,7 +45,15 @@
 
 })();
 
-	$(window).load(function() {
-		
-		$(".se-pre-con").fadeOut("slow");;
-	});
+	Swal.fire({
+  title: 'Custom width, padding, background.',
+  width: 600,
+  padding: '3em',
+  background: '#fff url(/images/trees.png)',
+  backdrop: `
+    rgba(0,0,123,0.4)
+    url("/images/nyan-cat.gif")
+    left top
+    no-repeat
+  `
+})
